7a5ff9a10ed4f5f51f15dedc15dfeb821e1b65b9
liwuqiong <liwuqiong@homtail.com>
2025-07-12 12:44:08 +0800

    07121244

--- _plugins/obsidian_comments.rb
+++ _plugins/obsidian_comments.rb
@@ -1,20 +1,41 @@
 # _plugins/obsidian_comments.rb
-module Jekyll
-  class ObsidianCommentConverter < Converter
-    safe true
-    priority :low
+require 'liquid'
+require 'redcarpet'
 
-    def matches(ext)
-      ext =~ /^\.md$/i
-    end
+# 自定义Markdown渲染器
+class ObsidianRenderer < Redcarpet::Render::HTML
+  def block_code(code, language)
+    # 处理代码块
+    super
+  end
 
-    def output_ext(ext)
-      ".html"
-    end
-
-    def convert(content)
-      # 将%%注释%%替换为HTML标签
-      content.gsub(/%%(.*?)%%/) { |match| "<span class=\"obsidian-comment\">#{$1}</span>" }
-    end
+  def paragraph(text)
+    # 检测并转换%%注释%%
+    text.gsub!(/%%(.*?)%%/) { |match| "<span class=\"obsidian-comment\">#{$1}</span>" }
+    "<p>#{text}</p>"
   end
 end
+
+# 注册自定义渲染器
+class ObsidianMarkdown < Liquid::Tag
+  def initialize(tag_name, text, tokens)
+    super
+    @text = text
+  end
+
+  def render(context)
+    # 获取配置
+    site = context.registers[:site]
+    converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+    
+    # 使用自定义渲染器
+    renderer = ObsidianRenderer.new(hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    
+    # 渲染Markdown
+    content = context[@text] || @text
+    markdown.render(content)
+  end
+end
+
+Liquid::Template.register_tag('obsidian_markdown', ObsidianMarkdown)
